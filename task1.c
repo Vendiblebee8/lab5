8b233e814003007d80e69722dc827f4803601c74
Vincenzo Cataneo <vcatane1@cs.binghamton.edu>
2023-11-01 14:43:19 -0400

    finished lab5

--- task1.c
+++ task1.c
@@ -8,12 +8,63 @@
 
 int main(void) {
     char * argv1[] = {"cat", "Makefile", 0};
-    char * argv2[] = {"head", "-4", 0};
-    //char * argv2[] = {"wc", "-l", 0};
+    //char * argv2[] = {"head", "-4", 0};
+    char * argv2[] = {"wc", "-l", 0};
     
     setbuf(stdout, NULL);
 
+    int fd[2];
+    pid_t child1, child2;
 
+    // Create a pipe
+    if (pipe(fd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    // Fork the first child process (C1)
+    if ((child1 = fork()) == -1) {
+        perror("fork C1");
+        exit(EXIT_FAILURE);
+    }
+
+    if (child1 == 0) {  // In child process C1
+        printf("In CHILD-1 (PID=%d): executing command %s ...\n", getpid(), argv1[0]);
+        close(fd[0]);  // Close read end of the pipe
+        dup2(fd[1], STDOUT_FILENO);  // Redirect standard output to write end of the pipe
+        close(fd[1]);  // Close the write end since we've redirected it
+        execvp(argv1[0], argv1);
+        // If execvp() fails
+        perror("execvp");
+        exit(EXIT_FAILURE);
+    }
+
+    // Fork the second child process (C2)
+    if ((child2 = fork()) == -1) {
+        perror("fork C2");
+        exit(EXIT_FAILURE);
+    }
+
+    if (child2 == 0) {  // In child process C2
+        printf("In CHILD-2 (PID=%d): executing command %s ...\n", getpid(), argv2[0]);
+        close(fd[1]);  // Close write end of the pipe
+        dup2(fd[0], STDIN_FILENO);  // Redirect standard input from read end of the pipe
+        close(fd[0]);  // Close the read end since we've redirected it
+        execvp(argv2[0], argv2);
+        // If execvp() fails
+        perror("execvp");
+        exit(EXIT_FAILURE);
+    }
+
+    // Close both ends of the pipe in the parent
+    close(fd[0]);
+    close(fd[1]);
+
+    // Wait for both child processes to finish
+    waitpid(child1, NULL, 0);
+    waitpid(child2, NULL, 0);
+    printf("In PARENT (PID=%d): successfully reaped child (PID=%d)\n", getpid(), child1);
+    printf("In PARENT (PID=%d): successfully reaped child (PID=%d)\n", getpid(), child2);
 
     return 0;
 }
--- task2.c
+++ task2.c
@@ -7,12 +7,68 @@
 #include <sys/wait.h>
 
 int main(void) {
-    char * argv1[] = {"cat", "Makefile", 0};
-    char * argv2[] = {"head", "-4", 0};
-    char * argv3[] = {"wc", "-l", 0};
+    char *argv1[] = {"cat", "Makefile", NULL};
+    char *argv2[] = {"head", "-4", NULL};
+    char *argv3[] = {"wc", "-l", NULL};
+    int fd1[2], fd2[2];
+    pid_t child1, child2, child3;
 
-    setbuf(stdout, NULL);
+    setbuf(stdout, NULL);  // to disable buffering
 
+    if (pipe(fd1) == -1 || pipe(fd2) == -1) {
+        perror("pipe");
+        exit(1);
+    }
+
+    // First child: cat Makefile
+    if ((child1 = fork()) == 0) {
+        printf("In CHILD-1 (PID=%d): executing command cat ...\n", getpid());
+        close(fd1[0]);
+        dup2(fd1[1], STDOUT_FILENO);
+        close(fd1[1]);
+        execvp(argv1[0], argv1);
+        perror("execvp");
+        exit(1);
+    }
+
+    // Second child: head -4
+    if ((child2 = fork()) == 0) {
+        printf("In CHILD-2 (PID=%d): executing command head ...\n", getpid());
+        close(fd1[1]);
+        dup2(fd1[0], STDIN_FILENO);
+        close(fd1[0]);
+
+        close(fd2[0]);
+        dup2(fd2[1], STDOUT_FILENO);
+        close(fd2[1]);
+        execvp(argv2[0], argv2);
+        perror("execvp");
+        exit(1);
+    }
+
+    close(fd1[0]);
+    close(fd1[1]);
+
+    // Third child: wc -l
+    if ((child3 = fork()) == 0) {
+        printf("In CHILD-3 (PID=%d): executing command wc ...\n", getpid());
+        close(fd2[1]);
+        dup2(fd2[0], STDIN_FILENO);
+        close(fd2[0]);
+        execvp(argv3[0], argv3);
+        perror("execvp");
+        exit(1);
+    }
+
+    close(fd2[0]);
+    close(fd2[1]);
+
+    // Parent waits for all children
+    int status;
+    pid_t wpid;
+    while ((wpid = wait(&status)) > 0) {
+        printf("In PARENT (PID=%d): successfully reaped child (PID=%d)\n", getpid(), wpid);
+    }
 
     return 0;
 }
